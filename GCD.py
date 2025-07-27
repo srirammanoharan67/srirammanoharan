4577859b5e239a7039c9d355122f4ea3154cfedb
srirammanoharan67 <144358395+srirammanoharan67@users.noreply.github.com>
2025-07-28 01:44:20 +0530

    updated something

--- GCD.py
+++ GCD.py
@@ -2,18 +2,18 @@ num1 = 0
 num2 = 0
 
 def gcd_func(num1,num2):
-    if num1 == 0 or num2 ==0 :
-        return 
+    if num1 == 0 or num2 ==0 : # Gcd cannot be zero
+        return 0
     if num1 > num2 :
         smallest = num2
     else :
         smallest = num1
-    print("smallest : ",smallest)
+    #print("smallest : ",smallest)
     for i in range(1,smallest+1):
         if num1%i==0 and num2%i==0 :
             gcd = i
     return gcd
-gcd_call = gcd_func(num1,num2)
+gcd_call = gcd_func(num1,num2) # Function call and return value
 
 if gcd_call:
     print(f"GCD of {num1} and {num2} is : {gcd_call}")
