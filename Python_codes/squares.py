5866e8889d4d0bd26664a9cc423d680a3f313870
srirammanoharan67 <144358395+srirammanoharan67@users.noreply.github.com>
2025-08-06 09:43:21 +0530

    square code  in various ways

--- Python_codes/squares.py
+++ Python_codes/squares.py
@@ -7,9 +7,13 @@ print(squares) # output : [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 for i in range(1,11):
     print(i*i,end=",") # output : 1,4,9,16,25,36,49,64,81,100,
 
-
+# Generates squares using while loop
 num = int(input("Enter the number of square : "))
 i =1
 while i <= num :
     print(i*i,end = ",") # output : 1,4,9,16,25,
     i+=1 # i = i + 1
+
+# Generates squares using Lambda Function
+square = lambda x : x**2
+print(square(5))
